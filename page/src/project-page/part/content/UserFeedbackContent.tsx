5812b407f0396f9f8f518b6f3c729860a722fc42
pascallinder <pascal.linder97@hotmail.com>
2024-07-27 09:11:05 +0200

    Update UserFeedbackContent.tsx

--- page/src/project-page/part/content/UserFeedbackContent.tsx
+++ page/src/project-page/part/content/UserFeedbackContent.tsx
@@ -3,10 +3,13 @@ import ImgUrl from '../../../assets/sus-chart.svg?react';
 export const UserFeedbackContent: React.FC = () => {
   return (
     <div>
-      During a testing session, several technical issues were identified, and
-      potential areas for improvement in both the User Interface and
-      collaborative features were highlighted through questionnaires. This is
-      reflected in the System Usability Scale (SUS) chart below (Average SUS
+      We conducted two user testing sessions, each targeting different user
+      roles: students and instructors. The evaluation aimed to assess the
+      system's usability, performance, user experience, and overall
+      effectiveness . During the testing session, several technical issues were
+      identified, and potential areas for improvement in both the User Interface
+      and collaborative features were highlighted through questionnaires. This
+      is reflected in the System Usability Scale (SUS) chart below (Average SUS
       score = 58.92, mean SUS score = 65.00).
       <ImgUrl className="w-full md:w-[75%] m-auto" />
       Despite these challenges, the testing affirmed the extension’s potential.
